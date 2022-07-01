64bf40076e3afd236ae24877128b92ffda8429d9
Rahul3012 <rahullokhande3012@gmail.com>
2022-07-01 10:19:31 +0530

    Selected menu highlight and apply forloop on coursesData

--- application/routes.py
+++ application/routes.py
@@ -6,7 +6,7 @@ from flask import render_template
 @app.route("/index")
 @app.route("/home")
 def index():
-    return render_template("index.html", login=True)
+    return render_template("index.html", index=True)
 
 @app.route("/login")
 def login():
@@ -14,8 +14,9 @@ def login():
 
 @app.route("/courses")
 def courses():
-    return render_template("courses.html", login=True)
+    coursesData = [{"courseID":"1111","title":"ReactJs 111","description":"Intro to ReactJs","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv ReactJs 201","description":"Advanced ReactJs Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
+    return render_template("courses.html", coursesData = coursesData, courses = True)
 
 @app.route("/register")
 def register():
-    return render_template("register.html", login=True)
+    return render_template("register.html", register=True)
