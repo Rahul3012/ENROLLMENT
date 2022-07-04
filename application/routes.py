3f4e5a494cbe15a145052790cfb09615b8457297
Rahul3012 <rahullokhande3012@gmail.com>
2022-07-04 10:11:00 +0530

    Login route and login template updated

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,7 @@
 from application import app, db
 from flask import render_template, request, json, Response
 from application.models import User, Course, Enrollment
+from application.forms import LoginForm, RegisterForm
 
 coursesData = [{"courseID":"1111","title":"ReactJs 111","description":"Intro to ReactJs","credits":"3","term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv ReactJs 201","description":"Advanced ReactJs Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
 
@@ -11,9 +12,10 @@ coursesData = [{"courseID":"1111","title":"ReactJs 111","description":"Intro to 
 def index():
     return render_template("index.html", index=True)
 
-@app.route("/login")
+@app.route("/login", methods=['GET','POST'])
 def login():
-    return render_template("login.html", login=True)
+    form = LoginForm()
+    return render_template("login.html", title='Login', form=form, login=True)
 
 @app.route("/courses/")
 @app.route("/courses/<term>")
--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    SECRET_KY = os.environ.get('secret_key') or 'test_secret'
+    SECRET_KEY = os.environ.get('secret_key') or 'test_secret'
     MONGODB_SETTINGS = {'db':'UTA_Enrollment', 
         'host': 'mongodb://localhost:27017/UTA_Enrollment'
     }
