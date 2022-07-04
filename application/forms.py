09136a05d7c0c18881d00363e264425babf3000c
Rahul3012 <rahullokhande3012@gmail.com>
2022-07-04 09:48:54 +0530

    Register form updated

--- application/forms.py
+++ application/forms.py
@@ -15,3 +15,4 @@ class RegisterForm(FlaskForm):
     password_confirm    =   StringField("Confirm Password", validators = [DataRequired()])
     first_name          =   StringField("First Name", validators = [DataRequired()])
     last_name           =   StringField("Last Name", validators = [DataRequired()])
+    submit              =   SubmitField("Register Now")
