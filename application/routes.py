c4b18dfe950058158805b28185e80529b0b4ec0f
Rahul3012 <rahullokhande3012@gmail.com>
2022-07-06 09:33:06 +0530

    Validation changes for Register pager

--- application/routes.py
+++ application/routes.py
@@ -28,9 +28,24 @@ def login():
 def courses(term="2019"):
     return render_template("courses.html", coursesData = coursesData, courses = True,term = term)
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("register.html", register=True)
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_id     = User.objects.count()
+        user_id     += 1
+
+        email       = form.email.data
+        password    = form.password.data
+        first_name  = form.first_name.data
+        last_name   = form.last_name.data
+
+        user = User(user_id=user_id, email=email, first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save()
+        flash("You are successfully registered!","success")
+        return redirect(url_for('index'))
+    return render_template("register.html",title="Register", form=form, register=True)
 
 @app.route("/enrollment", methods=["GET", "POST"])
 def enrollment():
